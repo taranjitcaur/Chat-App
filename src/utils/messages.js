3dbb7e96ccccba05a2a0578a2e01c69fb500abcb
Taranjit Kaur <taranjitcaur@gmail.com>
2020-11-19 21:11:21 -0500

    users and user rooms functionality

--- src/utils/messages.js
+++ src/utils/messages.js
@@ -1,18 +1,30 @@
-const generateMessage = (text) => {
+const generateMessage = (username, text) => {
     return {
+        username,
         text,
         createdAt: new Date().getTime()
     }
 }
-
-const generateLocationMessage = (url) => {
+const generateLocationMessage = (username, url) => {
     return {
+        username,
         url,
         createdAt: new Date().getTime()
     }
 }
 
+const getUsersInRoom = (room) => {
+    room = room.trim().toLowerCase()
+    return users.filter((user) => user.room === room)
+}
+
+const getUser = (id) => {
+    return users.find((user) => user.id === id)
+}
+
 module.exports = {
     generateMessage,
-    generateLocationMessage
+    generateLocationMessage,
+    getUsersInRoom,
+    getUser
 }
